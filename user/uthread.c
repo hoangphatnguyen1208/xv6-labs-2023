150d920de2665ee5ba06364e4b02d5028c65f0ac
Nguyen Hoang Phat <88542260+hoangphat120805@users.noreply.github.com>
2026-04-21 22:18:48 +0700

    Uthread: switching between threads

--- user/uthread.c
+++ user/uthread.c
@@ -12,13 +12,29 @@
 
 
 struct thread {
+  uint64     ra;
+  uint64     sp;
+  
+  uint64     s0;
+  uint64     s1;
+  uint64     s2;
+  uint64     s3;
+  uint64     s4;
+  uint64     s5;
+  uint64     s6;
+  uint64     s7;
+  uint64     s8;
+  uint64     s9;
+  uint64     s10;
+  uint64     s11;
+
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
-              
+extern void thread_switch(struct thread *, struct thread *);
+
 void 
 thread_init(void)
 {
@@ -60,6 +76,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(t, next_thread);
   } else
     next_thread = 0;
 }
@@ -74,6 +91,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->sp = (uint64)t->stack + STACK_SIZE;
+  t->ra = (uint64)func;
 }
 
 void 
